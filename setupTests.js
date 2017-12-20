f9a6931eea66ec13d9b153b06a6ced57fd000949
John Lemp <john@graavy.com>
2017-12-20 11:42:58 -0500

    Add unit tests

---
+++ setupTests.js
@@ -0,0 +1,7 @@
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+
+chai.use(chaiEnzyme())
+Enzyme.configure({ adapter: new Adapter() })
